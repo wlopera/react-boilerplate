b94a31c813b30819aea858be0c86b9a7cd64bfbd
William Lopera <lopera.william@gmail.com>
2021-05-08 11:16:38 -0500

    Incio dle proyecto

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { render } from "react-dom";
+
+import App from "./components/App";
+
+render(
+  <div>
+    <h1>Aplicación</h1>
+    <App />
+  </div>,
+  document.getElementById("root")
+);
